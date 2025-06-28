d7392b27e71b33b00f76496ad937532f9fcf3081
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-06-28 18:30:55 -0400

    Update gitignore file, create testing and exploration code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
+// src/index.ts
+import readline from "readline";
+
+// set up filesystem
+import fs from "fs";
+const DATA_FILE = "users.json";
+
+// check if data file exists
+if (!fs.existsSync(DATA_FILE)) {
+    fs.writeFileSync(DATA_FILE, "[]");
+}
+
 // testing setup
-console.log("Setup Successful!");
+//console.log("Setup Successful!");
+
+
+// testing interacting with command line
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("What is your name? ", (answer) => {
+    console.log(`Hello, ${answer}`);
+    rl.close();
+});
