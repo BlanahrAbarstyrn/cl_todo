6f9f27d3d0afbaf3b766aa39da8d85ca546a6e29
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-07-02 21:30:17 -0400

    working on creating json saved date file

--- src/data.ts
+++ src/data.ts
@@ -1,16 +1,29 @@
 // src/storage.ts
 
+import readline from "readline";
 import fs from "fs";
 import { User } from "./types";
 
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
 const DATA_FILE = "users.json";
 
+
 export function loadUsers(): User[] {
-    // read file, parse JSON, return array of User objects 
+    // read file, parse JSON, return array of User objects
+    if (fs.existsSync("users.json")) {
+        Users = JSON.parse(fs.readFileSync("users.json", "utf8"));
+    } 
 }
 
 export function saveUsers(users: User[]): void {
     // stringify to JSON, write to file
+    fs.writeFileSync(DATA_FILE, JSON.stringify(users, null, 2));
 }
 
 export function deleteUser(username: string): void {
@@ -20,9 +33,23 @@ export function deleteUser(username: string): void {
 }
 
 
+export function createUser(username: string): void {
+    rl.question("Enter new user name ", (answer) => {
+        console.log(`\nWelcome, ${answer}!`);
+    });
+    
+    // prompt user to type a user name
+    // compare input to existing users
+    // reprompt for another name if name exists
+    // else push name to users array
+    // immediately call create project
+}
+
+
 /*
 // check if data file exists
 if (!fs.existsSync(DATA_FILE)) {
     fs.writeFileSync(DATA_FILE, "[]");
 }
 */
+
--- src/index.ts
+++ src/index.ts
@@ -10,48 +10,79 @@ const rl = readline.createInterface({
 
 // Main Menu
 function printMainMenu() {
-    console.log("==== MAIN MENU =====");
+    console.log("\n==== MAIN MENU ====");
     console.log(" 1. Create User");
     console.log(" 2. Load User");
     console.log(" 3. Delete User");
     console.log(" 4. Help Menu");
     console.log(" 5. Quit");
-    console.log("===================");
+    console.log("===================\n");
 }
 
 function promptMenu() {
     rl.question("Enter numeric selection ", (answer) => {
         if (answer === "1") {
-            //call create user menu
-            console.log("...creating user...");
-            printMainMenu();
-            promptMenu();
+            console.log("\n...creating user...");
+            createUser();
         } else if (answer === "2") {
             // load user code here
-            console.log("...loading user...");
+            console.log("\n...loading user...");
             printMainMenu();
             promptMenu();
         } else if (answer === "3") {
             // deleting user code here
-            console.log("...deleting user...");
+            console.log("\n...deleting user...");
             printMainMenu();
             promptMenu();
         } else if (answer === "4") {
             // call the help menu
-            console.log("...help menu under construction...Sorry!");
+            console.log("\n...help menu under construction...Sorry!");
             printMainMenu();
             promptMenu();
         } else if (answer === "5") {
-            console.log("Goodbye!");
+            console.log("\nGoodbye!\n");
             rl.close();
             process.exit(0);
         } else {
-          console.log("Invalid option. Please try again.");
+          console.log("\nInvalid option. Please try again.");
           printMainMenu();
           promptMenu(); // ask again!
         }
     });
 }
+
+let Users: User[] = [];
+
+function createUser() {
+    rl.question("Enter new user name or enter c to cancel: ", (answer) => {
+        if (answer.toLowerCase() === "c") {
+            cancel();
+            return;
+        }
+        if (Users.some(u => u.username === answer)) {
+            console.log("Username already exists. ");
+            createUser();
+            return;
+        }
+        Users.push({ username: answer, projects: [] });
+        saveUsers(Users);
+        console.log(`\nWelcome, ${answer}!`);
+        console.log("All users:", Users.map(u => u.username).join(", "));
+        printMainMenu();
+        promptMenu();
+    });
+}
+
+
+function cancel() {
+    printMainMenu();
+    promptMenu();
+}
+
+
+//function saveUsers(users: User[]): void {
+    // stringify to JSON, write to file
+//}
 
 
 // main loop
